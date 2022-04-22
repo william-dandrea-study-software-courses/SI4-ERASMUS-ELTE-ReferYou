297741227d3a69f43aae42e07f02202c7c642343
DANDREA WILLIAM <william.d'andrea@etu.unice.fr>
2022-04-22 14:04:37 +0200

    Update and correct some buggs

--- cloud-gateway/src/main/java/com/referyou/cloud/gateway/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/com/referyou/cloud/gateway/CloudGatewayApplication.java
@@ -2,6 +2,7 @@ package com.referyou.cloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
