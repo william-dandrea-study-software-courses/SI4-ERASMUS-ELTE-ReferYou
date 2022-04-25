a12be036662d4c32624e7a0bf8dee5f43bbf4721
YannClodong <yann.clodong@gmail.com>
2022-04-25 10:11:55 +0200

    :construction: Create basic interface for Promotion

---
+++ promotion-service/src/main/java/com/referyou/promotionservice/controllers/PromotionController.java
@@ -0,0 +1,24 @@
+package com.referyou.promotionservice.controllers;
+
+import com.referyou.promotionservice.models.Promotion;
+import org.apache.commons.lang.NotImplementedException;
+import org.springframework.web.bind.annotation.*;
+
+@RestController("/promotion")
+public class PromotionController {
+
+    @PostMapping("/")
+    public Promotion CreatePromotion(Float value) {
+        throw new NotImplementedException();
+    }
+
+    @GetMapping("/{id}")
+    public Promotion GetPromotionById(@PathVariable Long id) {
+        throw new NotImplementedException();
+    }
+
+    @GetMapping("/")
+    public Promotion GetPromotionByCodeId(@RequestParam("codeId") Long codeId) {
+        throw new NotImplementedException();
+    }
+}
