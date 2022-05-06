f2248b7898738430f696c1de196ffc271e260f27
DANDREA WILLIAM <william.d'andrea@etu.unice.fr>
2022-05-06 13:03:35 +0200

    Correct bug

--- authentification-service/src/main/java/com/referyou/authentificationservice/AuthentificationServiceApplication.java
+++ authentification-service/src/main/java/com/referyou/authentificationservice/AuthentificationServiceApplication.java
@@ -2,8 +2,10 @@ package com.referyou.authentificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AuthentificationServiceApplication {
 
 	public static void main(String[] args) {
--- moderation-service/src/main/java/com/referyou/moderationservice/ModerationServiceApplication.java
+++ moderation-service/src/main/java/com/referyou/moderationservice/ModerationServiceApplication.java
@@ -2,8 +2,10 @@ package com.referyou.moderationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ModerationServiceApplication {
 
 	public static void main(String[] args) {
